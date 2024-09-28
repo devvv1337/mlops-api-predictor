33fedb935749a74bceed1f375b1fc0066dc82291
Julien Cardi <juliencardi@MacBook-Air-de-Julien.local>
2024-09-28 17:36:21 +0200

    Moved params.yaml to root directory and updated paths

--- app/model.py
+++ app/model.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import joblib
 import pandas as pd
-import yaml  # Importer PyYAML
+import yaml
 import os
 
 def load_params(params_path='params.yaml'):
@@ -13,11 +13,11 @@ def load_params(params_path='params.yaml'):
         params = yaml.safe_load(file)
     return params['train']
 
+
 def load_data():
-    # Chargement des données depuis le fichier CSV versionné par DVC
     data = pd.read_csv('data/raw/iris.csv')
-    X = data.drop('species', axis=1)  # Remplacement de 'target' par 'species'
-    y = data['species']               # Remplacement de 'target' par 'species'
+    X = data.drop('species', axis=1)
+    y = data['species']
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
 def train_and_log_model(params_path='params.yaml'):
@@ -25,27 +25,28 @@ def train_and_log_model(params_path='params.yaml'):
     X_train, X_test, y_train, y_test = load_data()
 
     with mlflow.start_run() as run:
-        # Entraîner le modèle avec les paramètres chargés
         clf = RandomForestClassifier(
             n_estimators=params['n_estimators'],
             random_state=params['random_state']
         )
         clf.fit(X_train, y_train)
 
-        # Évaluer le modèle
         accuracy = clf.score(X_test, y_test)
 
-        # Enregistrer les métriques et paramètres
         mlflow.log_param("n_estimators", params['n_estimators'])
         mlflow.log_param("random_state", params['random_state'])
         mlflow.log_metric("accuracy", accuracy)
 
-        # Enregistrer le modèle
         mlflow.sklearn.log_model(clf, "model")
 
-        # Sauvegarder le modèle localement
-        joblib.dump(clf, '/models/model.joblib')
+        # S'assurer que le répertoire 'models' existe
+        os.makedirs('../models', exist_ok=True)
+
+        # Enregistrer le modèle localement
+        joblib.dump(clf, '../models/model.joblib')
         print(f"Model trained and logged with run_id: {run.info.run_id}")
+
+
         return run.info.run_id
 
 if __name__ == "__main__":
